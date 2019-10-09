e7716e3c6e9914628c0aa5e00c89a0fe7362a695
Derek Musser <derek.musser@icloud.com>
2019-10-08 21:52:07 -0700

    added 404

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React, { memo } from 'react'
+import { Layout } from '../components'
+import { SEO } from 'elements'
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
+)
+
+export default memo(NotFoundPage)
