2a5cacbf49f646e719a52ced4680b863047c5f53
Derek Musser <derek.musser@icloud.com>
2021-03-30 06:21:17 -0700

    Split primatives off

---
+++ src/atoms/formAtoms.js
@@ -0,0 +1,72 @@
+import { atom } from 'jotai';
+import { atomWithReset } from 'jotai/utils';
+import { INITIAL_FORM, handleErrors, handleFormSubmission } from './utils';
+
+export const formAtom = atomWithReset(INITIAL_FORM);
+export const updateFormAtom = atom(
+	(get) => get(formAtom),
+	(get, set, event) =>
+		set(formAtom, { ...get(formAtom), [event.target.name]: event.target.value })
+);
+
+export const touchedAtom = atom([]);
+
+export const updateTouchedAtom = atom(
+	(get) => get(touchedAtom),
+	(get, set, name) => {
+		const touched = get(touchedAtom);
+		return set(
+			touchedAtom,
+			touched.indexOf(name) === -1
+				? [...touched, name]
+				: touched.splice(touched.indexOf(name), 1)
+		);
+	}
+);
+
+export const errorsAtom = atom([]);
+export const updateErrorsAtom = atom(
+	(get) => get(errorsAtom),
+	(get, set, name) => {
+		const errors = get(errorsAtom);
+		const touched = get(touchedAtom).indexOf(name) !== -1;
+		const fieldError = touched && handleErrors(get(formAtom), name);
+		return set(
+			errorsAtom,
+			errors.indexOf(name) === -1
+				? !fieldError
+					? errors.splice(errors.indexOf(name), 1)
+					: [...errors, name]
+				: errors
+		);
+	}
+);
+
+export const submittedAtom = atom(false);
+
+export const submitFormAtom = atom(
+	(get) => get(submittedAtom),
+	(get, set, _arg) => {
+		if (!get(errorsAtom)?.length) {
+			handleFormSubmission(get(formAtom)).then(() => {
+				set(formAtom, INITIAL_FORM);
+				return set(submittedAtom, true);
+			});
+		}
+	}
+);
+
+export const clicksAtom = atom(0);
+
+export const updateClicksAtom = atom(
+	(get) => get(clicksAtom),
+	(get, set, _arg) => {
+		const clicks = get(clicksAtom);
+		if (clicks < 2) {
+			return set(clicksAtom, clicks + 1);
+		} else {
+			set(submittedAtom, false);
+			return set(clicksAtom, 0);
+		}
+	}
+);
--- src/atoms/useAtomForm.js
+++ src/atoms/useAtomForm.js
@@ -1,71 +1,21 @@
-import { atom, useAtom } from 'jotai';
-import { atomWithReset, useResetAtom } from 'jotai/utils';
+import { useAtom } from 'jotai';
+import { useResetAtom } from 'jotai/utils';
 import {
-	INITIAL_FORM,
-	handleErrors,
-	handleFormSubmission,
-	createErrorMessage,
-} from './utils';
-
-const formAtom = atomWithReset(INITIAL_FORM);
-const updateFormAtom = atom(
-	(get) => get(formAtom),
-	(get, set, event) =>
-		set(formAtom, { ...get(formAtom), [event.target.name]: event.target.value })
-);
-
-const errorsAtom = atom([]);
-const updateErrorsAtom = atom(
-	(get) => get(errorsAtom),
-	(get, set, name) => {
-		const errors = get(errorsAtom);
-		const fieldError = handleErrors(get(formAtom), name);
-		const focus = !fieldError && errors.indexOf(name) !== -1;
-		return set(
-			errorsAtom,
-			focus
-				? errors.splice(errors.indexOf(name), 1)
-				: fieldError
-				? [...errors, name]
-				: errors
-		);
-	}
-);
-
-const submittedAtom = atom(false);
-
-const submitFormAtom = atom(
-	(get) => get(submittedAtom),
-	(get, set, _arg) => {
-		if (!get(errorsAtom)?.length) {
-			handleFormSubmission(get(formAtom)).then(() => {
-				set(formAtom, INITIAL_FORM);
-				return set(submittedAtom, true);
-			});
-		}
-	}
-);
-
-const clicksAtom = atom(0);
-
-const updateClicksAtom = atom(
-	(get) => get(clicksAtom),
-	(get, set, _arg) => {
-		const clicks = get(clicksAtom);
-		if (clicks < 2) {
-			return set(clicksAtom, clicks + 1);
-		} else {
-			set(submittedAtom, false);
-			return set(clicksAtom, 0);
-		}
-	}
-);
+	submitFormAtom,
+	updateClicksAtom,
+	updateFormAtom,
+	updateErrorsAtom,
+	updateTouchedAtom,
+	formAtom,
+} from './formAtoms';
+import { createErrorMessage } from './utils';
 
 const useAtomForm = () => {
 	const [submitted, handleSubmit] = useAtom(submitFormAtom);
 	const [clicks, handleClick] = useAtom(updateClicksAtom);
 	const [form, updateForm] = useAtom(updateFormAtom);
 	const [errors, updateErrors] = useAtom(updateErrorsAtom);
+	const [touched, updateTouched] = useAtom(updateTouchedAtom);
 	const handleReset = useResetAtom(formAtom);
 
 	return {
@@ -78,18 +28,20 @@ const useAtomForm = () => {
 			onChange: updateForm,
 			onFocus: (event) => {
 				const { name } = event.target;
-				updateErrors((errors) =>
-					errors?.length && errors.indexOf(name) !== -1
-						? errors.splice(errors.indexOf(name), 1)
-						: errors
-				);
+				if (touched.indexOf(name) !== -1) {
+					updateTouched(name);
+				}
+				updateErrors(name);
 			},
 			onBlur: (event) => {
 				updateForm(event);
-				updateErrors(event.target.name);
+				const { name } = event.target;
+				if (touched.indexOf(name) === -1) {
+					updateTouched(name);
+				}
+				updateErrors(name);
 			},
 		},
-
 		handleReset,
 		handleSubmit,
 		handleClick,
