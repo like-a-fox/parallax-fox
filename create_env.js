bf448e44ff884c8020aad18ab02aeb2722ce2bd3
Inanis Umbra <umbra@Umbra.local>
2021-03-30 05:25:45 -0700

    Removed env keys

--- create_env.js
+++ create_env.js
@@ -1,12 +1,5 @@
 const firebaseConfig = {
-	apiKey: 'AIzaSyCYC4_YR8nHblokKMWgHYPtjmvg0lisC-g',
-	authDomain: 'parallax-fox.firebaseapp.com',
-	databaseURL: 'https://parallax-fox.firebaseio.com',
-	projectId: 'parallax-fox',
-	storageBucket: 'parallax-fox.appspot.com',
-	messagingSenderId: '595587400052',
-	appId: '1:595587400052:web:f2dc5e1810f23c3f35ac21',
-	measurementId: 'G-GF6W9N8HQN',
+	/* FIREBASE CONFIG HERE */
 };
 
 const baseString = 'GATSBY_FIREBASE_';
